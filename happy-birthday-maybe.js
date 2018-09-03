0f8ab95ec4fdb88cc11fb28d8180ed6efb2dd982
ychi <ychi@users.noreply.github.com>
2018-09-03 23:06:05 +0800

    Create happy-birthday-maybe.js
    Wish c9s a happy birthday.

---
+++ happy-birthday-maybe.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class Maybe {
+
+  constructor (x) {
+    this.$val = x;
+  }
+  
+  static of (x) {
+    return new Maybe(x);
+  }
+  
+  get isNothing () {
+    return this.$val !== 'c9s';
+  }
+  
+  map(fn) {
+    return this.isNothing ? this : new Maybe.of(fn(this.$val));
+  }
+  
+  inspect() {
+    return this.isNothing ? 'Maybe(Nothing) : `Maybe(${inspect(this.$value)})`;
+  }
+}
+
+let birthdayGreeting = function (who) {
+  return 'Happy Birthday, ' + who + '🎉';
+};
+
+Maybe.of('c9s')
+.map(birthdayGreeting)
+.map(console.log);
